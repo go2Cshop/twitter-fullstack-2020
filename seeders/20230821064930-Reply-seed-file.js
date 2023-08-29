d02d50547bfcc940da3027a55e8dd4ade7865504
Easonlu0425 <andy1234532@hotmail.com>
2023-08-29 15:15:47 +0800

    modify seeder

--- seeders/20230821064930-Reply-seed-file.js
+++ seeders/20230821064930-Reply-seed-file.js
@@ -3,7 +3,7 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = await queryInterface.sequelize.query('SELECT id FROM Users WHERE email !="root@example.com"',
+    const users = await queryInterface.sequelize.query('SELECT id FROM Users WHERE email != \'root@example.com\'',
       { type: queryInterface.sequelize.QueryTypes.SELECT })
     const tweets = await queryInterface.sequelize.query('SELECT id FROM Tweets',
       { type: queryInterface.sequelize.QueryTypes.SELECT })
